26681547410a92af8001ee819c9b39b6b20ec53d
wzyabcas <ziyin@illinois.edu>
2026-02-13 11:39:07 -0600

    Evaluation_script

---
+++ text2interaction/scripts/eval_wguide.sh
@@ -0,0 +1,8 @@
+python -m eval.eval_marker_representation_guide \
+--model_path save/pretrained/model.pt \
+--motion_length 10 \
+--multi_backbone_split 4 \
+--layers 8 \
+--latent_dim 512 \
+--dataset interact \
+--debug 0
